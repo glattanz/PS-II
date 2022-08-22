c23cfdec87fe7af52b3c3e3f1981119ba25fe7ad
Gabrielle <g.lattanzi26@gmail.com>
2022-08-21 22:10:01 -0300

    Exercicios M2

---
+++ Exercicio3-M1/src/main/java/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+
+    private int codigo;
+    private String nome;
+
+    public void inserir(){
+        System.out.println("Inserindo no carrinho... ");
+    }
+
+    public void excluir(){
+        System.out.println("Excluindo do carrinho...");
+    }
+
+    public void localizar(){
+        System.out.println("Localizando produto...");
+    }
+}
---
+++ Exercicio3-M1/src/main/java/Fornecedor.java
@@ -0,0 +1,17 @@
+public class Fornecedor {
+
+    private String cnpj;
+    private String razaoSocial;
+
+    public void inserir(){
+        System.out.println("Inserindo novo produto... ");
+    }
+
+    public void excluir(){
+        System.out.println("Excluindo produto... ");
+    }
+
+    public void localizar(){
+        System.out.println("Localizando produto... ");
+    }
+}
